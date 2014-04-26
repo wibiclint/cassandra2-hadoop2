791f535e9ed4a26578284df933eb8a9235cf287a
Clint Kelly <clint@wibidata.com>
2014-04-26 13:27:31 -0700

    Added test case for combining two queries.

--- src/test/java/org/apache/cassandra/hadoop2/NativeInputFormat/BaseInputFormatTest.java
+++ src/test/java/org/apache/cassandra/hadoop2/NativeInputFormat/BaseInputFormatTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.DataType;
@@ -20,6 +21,7 @@ import com.datastax.driver.core.Session;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.service.EmbeddedCassandraService;
 import org.apache.commons.io.FileUtils;
@@ -43,7 +45,6 @@ public abstract class BaseInputFormatTest {
   protected static final String COL_TEAM = "team";
 
   protected static final String COL_LOGO = "logo";
-  protected static final String COL_NUMBER = "player_num";
   protected static final String COL_PLAYER = "player";
 
   protected static final int NUM_TEAMS = 8;
@@ -86,6 +87,8 @@ public abstract class BaseInputFormatTest {
       // Use a custom YAML file that specifies different ports from normal for RPC and thrift.
       //File yamlFile = new File(getClass().getResource("/cassandra.yaml").getFile());
       File yamlFile = new File(BaseInputFormatTest.class.getResource("/cassandra.yaml").getFile());
+
+      // TODO: Edit the YAML file to set up different directories, ports, etc. per test class?
 
       assert (yamlFile.exists());
       System.setProperty("cassandra.config", "file:" + yamlFile.getAbsolutePath());
@@ -144,7 +147,27 @@ public abstract class BaseInputFormatTest {
   }
 
   private static void createAndPopulatePlayerTable() {
+    mSession.execute(String.format(
+        "CREATE TABLE IF NOT EXISTS %s.%s (%s text, %s text, %s text, %s text, " +
+            "PRIMARY KEY(%s, %s, %s, %s))",
+        KEYSPACE, TABLE_PLAYERS,
+        COL_STATE, COL_CITY, COL_TEAM, COL_PLAYER,
+        COL_STATE, COL_CITY, COL_TEAM, COL_PLAYER));
 
+    // Let's insert some data for a few teams!
+    PreparedStatement preparedStatement = mSession.prepare(String.format(
+        "INSERT INTO %s.%s (%s, %s, %s, %s) VALUES (?, ?, ?, ?)",
+        KEYSPACE, TABLE_PLAYERS, COL_STATE, COL_CITY, COL_TEAM, COL_PLAYER));
+    for (TeamData teamData : mTeams.values()) {
+      for (String player : teamData.getPlayers()) {
+        mSession.execute(preparedStatement.bind(
+            teamData.getState(),
+            teamData.getCity(),
+            teamData.getTeamName(),
+            player
+        ));
+      }
+    }
   }
 
   private static void createTeams() {
@@ -158,6 +181,21 @@ public abstract class BaseInputFormatTest {
     mTeams.put("Spurs", new TeamData("TX", "San Antonio", "Spurs", "Spur"));
     mTeams.put("Rockets", new TeamData("TX", "Houston", "Rockets", "Big H"));
     Preconditions.checkArgument(NUM_TEAMS == mTeams.size());
+
+    mTeams.get("Bulls").addPlayer("Noah");
+    mTeams.get("Bulls").addPlayer("Butler");
+    mTeams.get("Wizards").addPlayer("Wall");
+    mTeams.get("Wizards").addPlayer("Beal");
+    mTeams.get("Clippers").addPlayer("Paul");
+    mTeams.get("Clippers").addPlayer("Griffin");
+    mTeams.get("Lakers").addPlayer("Kobe");
+    mTeams.get("Lakers").addPlayer("Gasol");
+    mTeams.get("Mavericks").addPlayer("Dirk");
+    mTeams.get("Mavericks").addPlayer("Monte");
+    mTeams.get("Spurs").addPlayer("Duncan");
+    mTeams.get("Spurs").addPlayer("Parker");
+    mTeams.get("Rockets").addPlayer("Harden");
+    mTeams.get("Rockets").addPlayer("Howard");
   }
 
   @BeforeClass
@@ -181,6 +219,7 @@ public abstract class BaseInputFormatTest {
     private final String mCity;
     private final String mTeamName;
     private final String mLogo;
+    private Set<String> mPlayers;
 
     // TODO: List of players.
 
@@ -189,11 +228,17 @@ public abstract class BaseInputFormatTest {
       mCity = city;
       mTeamName = teamName;
       mLogo = logo;
+      mPlayers = Sets.newHashSet();
+    }
+
+    public void addPlayer(String player) {
+      mPlayers.add(player);
     }
 
     public String getState() { return mState; }
     public String getCity() { return mCity; }
     public String getTeamName() { return mTeamName; }
     public String getLogo() { return mLogo; }
+    public Set<String> getPlayers() { return mPlayers; }
   }
 }
--- src/test/java/org/apache/cassandra/hadoop2/NativeInputFormat/TestRecordReader.java
+++ src/test/java/org/apache/cassandra/hadoop2/NativeInputFormat/TestRecordReader.java
@@ -7,10 +7,15 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.util.List;
 
+import com.datastax.driver.core.ColumnDefinitions;
 import com.datastax.driver.core.Row;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestRecordReader extends BaseInputFormatTest {
+  private static final Logger LOG = LoggerFactory.getLogger(TestRecordReader.class);
+
   private CqlInputSplit getCqlInputSplit() {
     // Create a single token range for the entire ring.
     Subsplit subsplit = Subsplit.createFromHost(
@@ -105,4 +110,107 @@ public class TestRecordReader extends BaseInputFormatTest {
     }
   }
 
+  @Test
+  public void testTwoQueries() {
+    // Select everything from logos and everything from players.
+    NewCqlConfigHelper.setInputCqlQuery(mConf, KEYSPACE, TABLE_LOGOS, COL_LOGO);
+    NewCqlConfigHelper.setInputCqlQuery(mConf, KEYSPACE, TABLE_PLAYERS, COL_PLAYER);
+    CqlRecordReader recordReader = new CqlRecordReader();
+
+    try {
+      recordReader.initializeWithConf(getCqlInputSplit(), mConf);
+
+      // Partition key here is just the state.
+      CqlQuerySpec querySpec = NewCqlConfigHelper.getInputCqlQueries(mConf).get(0);
+      List<String> partitioningKeys = recordReader.getPartitioningKeysForQuery(querySpec);
+      assertEquals(1, partitioningKeys.size());
+
+      // The RecordReader should group by primary key, so we should see a different List<Row> for
+      // each unique state.
+      int stateCount = 0;
+      while(true) {
+        if (!recordReader.nextKeyValue()) {
+          break;
+        }
+        stateCount += 1;
+      }
+      assertEquals(NUM_STATES, stateCount);
+
+    } catch (IOException ioe) {
+      assertFalse("Should not get here.", false);
+    }
+  }
+
+  @Test
+  public void testTwoQueriesWithClusteringColumns() {
+    // Select from logos and from players.
+    NewCqlConfigHelper.setInputCqlQuery(mConf, KEYSPACE, TABLE_LOGOS, COL_LOGO);
+    NewCqlConfigHelper.setInputCqlQuery(mConf, KEYSPACE, TABLE_PLAYERS, COL_PLAYER);
+
+    // Cluster by team!
+    NewCqlConfigHelper.setInputCqlQueryClusteringColumnsCsv(mConf, COL_CITY, COL_TEAM);
+    CqlRecordReader recordReader = new CqlRecordReader();
+
+    try {
+      recordReader.initializeWithConf(getCqlInputSplit(), mConf);
+
+      // Partition key here is just the state.
+      CqlQuerySpec querySpec = NewCqlConfigHelper.getInputCqlQueries(mConf).get(0);
+      List<String> partitioningKeys = recordReader.getPartitioningKeysForQuery(querySpec);
+      assertEquals(1, partitioningKeys.size());
+
+      // The RecordReader should group by team, so we should see a different List<Row> for each
+      // team.
+      int teamCount = 0;
+      boolean haveSeenWizards = false;
+      while(true) {
+        if (!recordReader.nextKeyValue()) {
+          break;
+        }
+        List<Row> rowsThisTeam = recordReader.getCurrentValue();
+
+        // If this is the Wizards, then we should see Wall and Beal.
+        Row row0 = rowsThisTeam.get(0);
+        if (row0.getString(COL_TEAM).equals("Wizards")) {
+          assertFalse(haveSeenWizards);
+          haveSeenWizards = true;
+
+          // We should have one row for John Wall, one for Bradley Beal, and one for the logo.
+          assertEquals(3, rowsThisTeam.size());
+
+          boolean haveSeenWall = false;
+          boolean haveSeenBeal = false;
+          boolean haveSeenLogo = false;
+
+          // Check to make sure that we get the rows that we expect.
+          for (Row row : rowsThisTeam) {
+            ColumnDefinitions columns = row.getColumnDefinitions();
+            if (columns.contains(COL_LOGO) && row.getString(COL_LOGO).equals("Wizard")) {
+              assertFalse(haveSeenLogo);
+              haveSeenLogo = true;
+            }
+
+            if (columns.contains(COL_PLAYER) && row.getString(COL_PLAYER).equals("Wall")) {
+              assertFalse(haveSeenWall);
+              haveSeenWall = true;
+            }
+
+            if (columns.contains(COL_PLAYER) && row.getString(COL_PLAYER).equals("Beal")) {
+              assertFalse(haveSeenBeal);
+              haveSeenBeal = true;
+            }
+          }
+          assertTrue(haveSeenBeal);
+          assertTrue(haveSeenLogo);
+          assertTrue(haveSeenWall);
+        }
+        teamCount += 1;
+      }
+      assertTrue(haveSeenWizards);
+      assertEquals(NUM_TEAMS, teamCount);
+
+    } catch (IOException ioe) {
+      assertFalse("Should not get here.", false);
+    }
+  }
 }
